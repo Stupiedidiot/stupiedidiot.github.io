dc83028a0ba9e46e5759b87acef4f1351d30a8df
Stupiedidiot <anothernoobeyneko@gmail.com>
2025-07-27 15:53:47 +0800

    added button to remove message

--- demo/main.js
+++ demo/main.js
@@ -7,15 +7,21 @@ demo_message.innerHTML =`
     top: 0;
     position: sticky;
     z-index: 100;
-    background-color: white;
     color: black;
+    background-color: white;
     border-bottom: 3px solid black;
     padding: 1rem;
     > span { font-weight: bolder; font-family: var(--display-font); }
     > a { text-decoration: none; color: lightgray; font-family: var(--display-font); }
     > a:hover { color: #c02832; }
+    > button { position:absolute; right:1rem; }
 }
 </style>
 <span>Attention:</span> This page is a demo!! - <a href="https://stupied-portfolio.neocities.org/">✮ head back »</a>
+<button onclick="removeMessage()">X</button>
 `
 document.getElementsByTagName('body')[0].prepend(demo_message)
+
+function removeMessage (){
+    event.target.parentNode.remove()
+}
